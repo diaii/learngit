eae9a9513b6f71ac23b653e996ad7342452e9853
diaii <2328919434@qq.com>
2021-08-30 15:32:24 +0800

    add a python file

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author       : 小鹿撞路
+Date         : 2021-08-30 15:31:26
+LastEditTime : 2021-08-30 15:31:27
+LastEditors  : 小鹿撞路
+Description  : hahaha
+FilePath     : \learngit\test.py
+self_motto   : 倘若自己都不为自己活出自己的人生，那还有谁会为自己而活呢。
+'''
+print('hello world')
